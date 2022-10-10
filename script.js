fa12acb4df4e6a6cbdbefbe8073b9e0f9bc49938
root <root@DESKTOP-1N84NER.localdomain>
2022-10-10 12:05:20 +0200

    added new files

---
+++ script.js
@@ -0,0 +1,92 @@
+const odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31];
+const randoms = [
+  3.0, 7.3, 1.01, 0.3, 0.05, 1.4, 9, 8, 7.77, 3.2, 12.4, 18.9, 15.0, 21.4,
+];
+const names = [
+  "Reinhold",
+  "Mathias",
+  "Hans",
+  "Ludwig",
+  "Walter",
+  "Joseph",
+  "Munther",
+];
+
+// Loops
+// Samo z uporabo operatorjev (for, while)
+
+// 1. Define functions
+// console.log("last", last(5, odds)); // => [23,25,27,29,31]
+// console.log("from", from(3, 6, odds)); // => [7,9,11,13]
+// console.log(
+//   "rest",
+//   rest((x) => x > 10, odds)
+// ); // => [11,13,15,17,19,21,23,25,27,29,31]
+// console.log("has", has(13, odds)); // => true
+// console.log("has", has(24, odds)); // => false
+
+// z loopi (for, while) je treba definirat te funkcije, ki so uporabljene v console.logih tako, da returnajo kar je tud napisan zraven in da delujejo tako kot so invokane.
+
+//*1.
+function last(num, odds) {
+  const array = [];
+  for (let i = odds.length - num; i < odds.length; i++) {
+    while (i < odds.length) {
+      array.push(odds[i]);
+      i++;
+    }
+  }
+  return array;
+}
+
+console.log("last", last(5, odds)); // => [23,25,27,29,31]
+
+//*2.
+function from(x, y, odds) {
+  const array = [];
+
+  for (let i = x; i <= y; i++) {
+    while (i >= x && i <= y) {
+      array.push(odds[i]);
+      i++;
+    }
+  }
+  return array;
+}
+console.log("from", from(3, 6, odds)); // => [7,9,11,13]
+
+//*3.
+function has(num, odds) {
+  for (let odd of odds) {
+    if (odd === num) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log("has", has(13, odds)); // => true
+
+//*4.
+function has1(num, odds) {
+  for (let odd of odds) {
+    if (odd === num) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log("has", has1(24, odds)); // => false
+
+//*5.
+function rest(x, odds) {
+  const array = [];
+
+  for (let i = 0; i < odds.length; i++) {}
+}
+
+console.log(
+  "rest",
+  rest((x) => x > 10, odds)
+); // => [11,13,15,17,19,21,23,25,27,29,31]

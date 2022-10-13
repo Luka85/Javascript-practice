2c2792b91fe48751750b8c9e7c66c68176df1d05
Luka Kikelj <kikelj.luka@gmail.com>
2022-10-13 14:27:34 +0200

    add instructions for exercise #4

--- script.js
+++ script.js
@@ -229,3 +229,12 @@ console.log(
   "myFilter",
   myFilter((x) => x == "Ludwig", names)
 ); // => ['Ludwig']
+
+//! 4. Define funcions
+console.log(map(odds, (x) => x + 1)); // => [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32]
+console.log(filter(odds, (x) => x < 6)); // => [1, 3, 5]
+console.log(some(odds, (x) => x > 20)); // => true
+console.log(every(odds, (x) => x > 20)); // => false
+
+// Can't use Array.map(), filter, some, every ..
+// Only with For || while
